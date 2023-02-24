654b386e43e849a91b151efbe77afb24709f319f
Leon Mainusch <leon.mainusch@web.de>
2023-02-24 18:27:33 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,8 @@ export default {
     // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
     ssr: false,
 
+    publicPath: '/CADI-Translation-Editor/',
+
     target: 'static',
 
     components: {
