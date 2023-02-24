6a7afbc6ae5627421ed5cf60da62189b7ef51c05
Leon Mainusch <leon.mainusch@web.de>
2023-02-24 17:02:42 +0100

    NEW: read json file

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,26 @@
 <template>
-    <div>
-        test
-    </div>
+  <div>
+    <input type="file" id="fileInput" accept=".json" />
+  </div>
 </template>
+
+<script>
+export default {
+  mounted() {
+    document
+      .getElementById("fileInput")
+      .addEventListener("change", function selectedFileChanged() {
+        if (this.files.length === 0) {
+          console.log("No file selected.");
+          return;
+        }
+        const reader = new FileReader();
+        reader.onload = function fileReadCompleted() {
+          // when the reader is done, the content is in reader.result.
+          console.log(reader.result);
+        };
+        reader.readAsText(this.files[0]);
+      });
+  },
+};
+</script>
